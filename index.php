424f75f6df505fd6989fbbd15896b0acf8e33a8d
Olivier Murat <oliv.murat@gmail.com>
2014-10-30 12:15:24 +0100

    Hello world!

--- index.php
+++ index.php
@@ -1,10 +1,1 @@
-<html>
-    <head>
-        <title>Hello World en PHP</title>
-    </head>
-    <body>
-        <p>
-            <?php echo 'Hello World !'; ?>
-        </p>
-    </body>
-</html>
+<?php echo 'Hello World !'; ?>

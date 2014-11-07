38b0971fb930e434acb16cd37fefbc61b3c73ecc
Olivier Murat <oliv.murat@gmail.com>
2014-11-07 10:09:58 +0100

    part2

--- index.php
+++ index.php
@@ -1,21 +1,11 @@
 <?php
-    
-    // It is a controller
-    
-    $link = mysql_connect("localhost");
-
-    mysql_select_db('base de donnée', $link);
-    
-    $result = mysql_query('SELECT id, title FROM post', $link);
-    
-    $posts = array();
-    
-    while ($row = mysql_fetch_assoc($result)) {
-        $posts[] = $row;
-    }
-
-    require 'templates/home.php';
-
-    mysql_close($link);
-
+	$link = mysql_connect("localhost");
+	mysql_select_db('base de donnée', $link);
+	$result = mysql_query('SELECT id, title FROM post', $link);
+	$posts = array();
+	while ($row = mysql_fetch_assoc($result)) {
+	    $posts[] = $row;
+	}
+	require 'templates/home.php';
+	mysql_close($link);
 ?>

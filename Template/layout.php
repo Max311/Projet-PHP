9fcc0251dde2084b460148f5c1d817624bcf2432
Olivier Murat <oliv.murat@gmail.com>
2014-11-14 09:24:29 +0100

    part5

--- Template/layout.php
+++ Template/layout.php
@@ -1,7 +1,15 @@
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-
-	<title> Morpion </title>
-	<meta http-equiv="Content-type" content="text/html; charset=iso-8859-1" />
-	<script type= "text/javascript" src="public/js/morpion.js"></script>
+	<head>
+	
+		<title> Morpion </title>
+		<meta http-equiv="Content-type" content="text/html; charset=iso-8859-1" />
+		<script type= "text/javascript" src="public/js/morpion.js"></script>
+		
+	</head>
+	<body>
+	
+		<?php echo $content ?>
+		
+	</body>
+</html>

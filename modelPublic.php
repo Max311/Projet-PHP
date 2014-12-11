ae68edfda951f4de58dfe21f766cde85d0e3a008
Olivier Murat <oliv.murat@gmail.com>
2014-12-11 09:36:34 +0100

    1

--- modelPublic.php
+++ modelPublic.php
@@ -38,8 +38,8 @@
 	}
 	recupDonnees();
 	
-	$req = $bdd->prepare('SELECT nom FROM jeux_video WHERE possesseur = ? AND prix <= ?');
-	$req->execute(array($_GET['possesseur'], $_GET['prix_max']));
+	$req = $bdd->prepare('SELECT Question FROM reponsesQuestionnaire = ?');
+	$req->execute(array($_GET['']));
 	
 	
 	

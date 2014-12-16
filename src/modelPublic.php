9f77e03bfe7ccc5ad4a14ae3c06754f589ba26af
Olivier Murat <oliv.murat@gmail.com>
2014-12-16 08:23:01 +0100

    @

--- src/modelPublic.php
+++ src/modelPublic.php
@@ -8,45 +8,42 @@ error_reporting(E_ALL);
 		$tab = $req->fetch(PDO::FETCH_ASSOC);
 		$req = $bdd->query('SELECT COUNT(*) AS nbrLignes FROM questionnaire');
 		$data = $req->fetch();
-		$oui = 0;
-		$non = 0;
 		
 		foreach ($_POST as $key=>$value)
 		{
 			if ($value == 'oui')
 
 			{	
-				$oui = $oui+1;
 				if ($data['nbrLignes'] == 0)
 				{
 					$req = $bdd->prepare ('INSERT INTO questionnaire(question,oui,non) VALUES (:question, :oui, :non)');
 					$req->execute(array('question' => $key,'oui' => 1,'non' => 0 ));
-					echo "1";
 				}
 				else
 				{
+					$req=$bdd->prepare('SELECT oui from questionnaire where question = :key');
+					$req->execute(array('key' => $key));
+					$value = $req->fetch()['oui'];
 					$req=$bdd->prepare('UPDATE questionnaire SET oui = :value WHERE question = :key');
-					$req->execute(array('value' => $oui, 'key' => $key));
-					echo "2";
+					$response = $req->execute(array(':value' => $value+1, ':key' => $key));
 				}
 			}
 			elseif ($value == 'non')
 			{
-				$non = $non+1;
 				if ($data['nbrLignes'] == 0)
 				{
 					$req = $bdd->prepare ('INSERT INTO questionnaire (question,oui,non) VALUES (:question, :oui, :non)');
 					$req->execute(array('question' => $key,'non' => 1,'oui' => 0 ));
-					echo "3";
 				}
 				else
 				{
+					$req=$bdd->prepare('SELECT non from questionnaire where question = :key');
+					$req->execute(array(':key' => $key));
+					$value = $req->fetch()['non'];
 					$req=$bdd->prepare('UPDATE questionnaire SET non = :value WHERE question = :key');
-					$req->execute(array('value' => $non, 'key' => $key));
-					echo "4";
+					$response = $req->execute(array(':value' => $value+1, ':key' => $key));
 				}
 			}
-			echo $key.$value.'<br>';
 		}
 	}
 	recupDonneesBDD();
